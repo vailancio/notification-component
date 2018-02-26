60c260039898fd0e1aaa2f1143e39a021ce45db9
Vailancio Rodrigues <vailancio248@gmail.com>
2018-02-26 14:51:54 +0530

    Added web socket but very buggy

--- src/app/notifications/notification.service.ts
+++ src/app/notifications/notification.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { QueueingSubject } from 'queueing-subject'
+import { Observable } from 'rxjs/Observable'
+import websocketConnect from 'rxjs-websockets'
+import 'rxjs/add/operator/share'
 import { Notification } from './notification';
 import { NotificationType } from './notification';
 import { Task } from './task';
@@ -7,20 +11,30 @@ import { Questionnaire } from './questionnaire';
 
 @Injectable()
 export class NotificationService {
-  notifications:Notification[];
-
-  constructor() { 
-  	this.notifications =[];
+  private inputStream: QueueingSubject<string>
+  public messages: Observable<string>
+  connect() {
+    if (this.messages)
+      return
+ 
+    // Using share() causes a single websocket to be created when the first
+    // observer subscribes. This socket is shared with subsequent observers
+    // and closed when the observer count falls to zero.
+    this.messages = websocketConnect(
+      'ws://127.0.0.1:8999',
+      this.inputStream = new QueueingSubject<string>()
+    ).messages.share()
   }
 
-  loadData(){
-  	this.notifications.push(new Notification(NotificationType.TASK_ASSIGNED, new Task('Book Travel', 'Oliver Quiver')));
-  	this.notifications.push(new Notification(NotificationType.TASK_ASSIGNED, new Task('Mobility Submit', 'Buck Owen')));
-  	this.notifications.push(new Notification(NotificationType.TASK_ASSIGNED, new Task('Compile task', 'Buck Owen')));
 
-    this.notifications.push(new Notification(NotificationType.QUESTIONNAIRE_COMPLETED, new Questionnaire("UX Designer Requistion", "Sarah Gutierrez")));
+  // loadData(){
+  // 	this.notifications.push(new Notification(NotificationType.TASK_ASSIGNED, new Task('Book Travel', 'Oliver Quiver')));
+  // 	this.notifications.push(new Notification(NotificationType.TASK_ASSIGNED, new Task('Mobility Submit', 'Buck Owen')));
+  // 	this.notifications.push(new Notification(NotificationType.TASK_ASSIGNED, new Task('Compile task', 'Buck Owen')));
 
-  	this.notifications.push(new Notification(NotificationType.REMINDER, new Reminder('Been has been on hold for 90 days', new Date(Date.parse("March 21, 2018")))));
+  //   this.notifications.push(new Notification(NotificationType.QUESTIONNAIRE_COMPLETED, new Questionnaire("UX Designer Requistion", "Sarah Gutierrez")));
 
-  }
+  // 	this.notifications.push(new Notification(NotificationType.REMINDER, new Reminder('Been has been on hold for 90 days', new Date(Date.parse("March 21, 2018")))));
+
+  // }
 }
--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Subscription } from 'rxjs/Subscription';
 import { NotificationService } from './notification.service';
 import { Notification } from './notification';
+import { NotificationType } from './notification';
 import { NotificationCardComponent } from './notification-card/notification-card.component';
+
+import { Task } from './task';
 
 @Component({
   selector: 'notifications',
@@ -11,6 +15,8 @@ import { NotificationCardComponent } from './notification-card/notification-card
 })
 export class NotificationsComponent implements OnInit {
   notifications: Notification[];
+
+  private socketSubscription: Subscription;
   private _assignedTaskCount: number = 0;
   private _remindersCount: number = 0;
   private _generalNotificationsCount: number = 0;
@@ -20,12 +26,28 @@ export class NotificationsComponent implements OnInit {
   @Output() onGeneralNotificationCountChange = new EventEmitter<number>();
 
   constructor(private notificationService: NotificationService) { 
-
+      this.notifications = [];
   }
 
   ngOnInit() {
-  	this.notificationService.loadData();
-  	this.notifications = this.notificationService.notifications;
+  	// this.notificationService.loadData();
+  	// this.notifications = this.notificationService.notifications;
+
+    this.notificationService.connect()
+ 
+    this.socketSubscription = this.notificationService.messages.subscribe((message: string) => {
+      console.log('Received notification from server: ', JSON.parse(message));
+      let t = JSON.parse(message);
+
+      if(t.type = "TASK_ASSIGNED"){
+        console.log(t.resource.title, t.assigned_by);
+        this.notifications.push(new Notification(NotificationType.TASK_ASSIGNED, new Task(t.resource.title, t.assigned_by)));
+      }
+
+      // if(message.type = "TASK_ASSIGNED"){
+          
+      // }
+    });
   }
 
   onNotificationReceived(notification: Notification){
@@ -36,6 +58,10 @@ export class NotificationsComponent implements OnInit {
       this.onAssignedTaskCountChange.emit(this._assignedTaskCount);
       this.onReminderCountChange.emit(this._remindersCount);
       this.onGeneralNotificationCountChange.emit(this._generalNotificationsCount);
+  }
+
+  ngOnDestroy() {
+    this.socketSubscription.unsubscribe()
   }
 
 }
